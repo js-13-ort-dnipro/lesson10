5d685b46d98c3d7f2a6d7fee5e0e70c23f55a331
Anatoliy Kigel <anatoliy.kigel@gmail.com>
2021-12-08 10:04:15 +0200

    Upload Files & Source

---
+++ src/app-load-imitation/assets/js/app.js
@@ -0,0 +1,3 @@
+
+    import { createApp } from "../../node_modules/vue/dist/vue.esm-browser.js";
+    
---
+++ src/demo-template/assets/js/app.js
@@ -0,0 +1,4 @@
+
+    const countriesURL = 'https://restcountries.com/v3.1/all';
+
+    const covidURL = 'https://api.covid19api.com/summary';
